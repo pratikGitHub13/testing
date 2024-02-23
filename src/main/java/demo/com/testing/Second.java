1db539fc2b094645a4797e836c43243412fd95c7
pratik_patrikar <pratik_patrikar@NGL001187.persistent.co.in>
2024-02-23 22:57:35 +0530

    Commiting Second file to BranchX

---
+++ src/main/java/demo/com/testing/Second.java
@@ -0,0 +1,25 @@
+package demo.com.testing;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium Pratice From Start\\drivers\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.demoblaze.com/index.html#");
+		
+		driver.manage().window().maximize();
+		
+	}
+
+}
